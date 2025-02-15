cd0e94bf21844c555c744ad88788a3150cbea218
Jackal-Studios <sashaluchyn@gmail.com>
2025-02-15 03:58:45 -0800

    py set stuff

--- python/crunchflow.py
+++ python/crunchflow.py
@@ -1,19 +1,36 @@
+import os
+import time
+import random, string
+
 
 def hello():
     print("hi")
 
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+def generate_unique_filename():
+    return str(current_milli_time()) + randomword(10)
+
 def create_input_file(input_data, filename):
     print("input file creating...")
+    
 
 def parse_output(filename):
     print("getting output")
+
+    #parse output data
+
     # clean up
+    os.remove(filename)
     return {}
 
 def get_output(input_data):
-    filename = "uniquename"
+    filename = generate_unique_filename()
     create_input_file(input_data, filename)
     print("input file created")
-    # run crunchtop
-
-    return parse_output(filename)
+    return parse_output(filename)    # run crunchtop
