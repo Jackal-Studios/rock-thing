59de830f28c97dea18945f373ac1138941bd38eb
Jackal-Studios <sashaluchyn@gmail.com>
2025-02-15 01:38:53 -0800

    readme and secret shit

--- python/app.py
+++ python/app.py
@@ -1,30 +1,7 @@
-# from flask import Flask
-# from .routes import main_routes  # Importing routes (views)
-# from flask import render_template, Blueprint
-
-# main_routes = Blueprint('main', __name__)
-
-# @main_routes.route('/')
-# def home():
-#     return render_template('home.html')
-
-# @main_routes.route('/about')
-# def about():
-#     return render_template('about.html')
-
-# app = Flask(__name__)
-
-# # @app.route('/')
-# # def hello_world():
-# #     return 'Hello, World!'
-
-# if __name__ == '__main__':
-#     # app.config.from_object('config.Config')
-#     app.register_blueprint(main_routes)
-#     app.run(debug=True)
-
 
 from app import create_app
+
+
 
 app = create_app()
 
--- python/routes.py
+++ python/routes.py
@@ -2,9 +2,17 @@ from flask import render_template, Blueprint
 
 main_routes = Blueprint('main', __name__)
 
+# def load_api_key():
+#     with open('.secrets', 'r') as secrets_file:
+#         for line in secrets_file:
+#             if line.startswith('API_KEY'):
+#                 return line.split('=')[1].strip()
+#     return None
+
 @main_routes.route('/')
 def home():
     return render_template('home.html')
+    
 
 # @main_routes.route('/tst')
 # def about():
